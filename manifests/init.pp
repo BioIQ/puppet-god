561715bfde3221c4804bc9a1c1bec47529089e4e
Léo POIROUX <leo.poiroux@gmail.com>
2014-06-04 15:27:31 +0200

    Add the ability to set custom params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
-class god inherits god::params {
+class god(
+  $root_path       = $god::params::root_path,
+  $global_config   = $god::params::global_config,
+  $config_dir_path = $god::params::config_dir_path,
+  $run_dir         = $god::params::run_dir,
+  $log_dir         = $god::params::log_dir,
+  $init_script     = $god::params::init_script,
+  $service         = $god::params::service,
+  $rbenv           = $god::params::rbenv,
+  $rbenv_shim_path = $god::params::rbenv_shim_path,
+  $binary_path     = $god::params::binary_path
+) inherits god::params {
   file { $root_path :
     ensure => directory,
     mode   => 0777
